1c304ed2d5055971b5d8486b7efd0ecb316faff7
Dylan Havelock <dylanhavelock@dylans-mbp-2.home>
2025-05-12 20:57:05 -0400

    vulns

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
-// Use bodyParser middleware to parse JSON and URL-encoded request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Handle POST requests to the root endpoint
 app.post('/', (req, res) => {
-  // Log the request body to the console (for debugging)
   console.log('Received body:', req.body);
 
-  // Send the request body back as the response
   res.json(req.body);
 });
 
