2bd79841c2ba7091c86120d2bc655fd76c430893
Dylan Havelock <dylanhavelock@dylans-mbp-2.home>
2025-05-12 21:01:43 -0400

    add reachable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import marked from 'marked';
 
 const app = express();
 const port = 3000;
@@ -10,7 +11,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/', (req, res) => {
   console.log('Received body:', req.body);
 
-  res.json(req.body);
+  // Make the vulnerable 'marked' package reachable:
+  const markdown = marked(req.body.input);
+
+  res.json({ output: markdown });
 });
 
 app.listen(port, () => {
